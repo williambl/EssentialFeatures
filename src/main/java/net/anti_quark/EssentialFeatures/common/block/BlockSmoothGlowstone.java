2c14b216043a2efc6e845fbaa6c0f3ac28d83002
williambl <william@williambl.com>
2016-07-28 19:18:16 +0100

    Added Smooth Flowstone!
    Texture by /u/Jake_Jeremy

---
+++ src/main/java/net/anti_quark/EssentialFeatures/common/block/BlockSmoothGlowstone.java
@@ -0,0 +1,33 @@
+package net.anti_quark.EssentialFeatures.common.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockSmoothGlowstone extends Block {
+
+	public BlockSmoothGlowstone(String registryName, Material material, float hardness, float resistance) {
+        super(material);
+        this.setCreativeTab(CreativeTabs.REDSTONE);
+        this.setHardness(hardness);
+        this.setResistance(resistance);
+        this.setRegistryName(registryName);
+        this.setUnlocalizedName(this.getRegistryName().toString());
+        this.setLightLevel(1F);
+        GameRegistry.register(this);
+        GameRegistry.register(new ItemBlock(this), getRegistryName());
+	}
+	
+    @SideOnly(Side.CLIENT)
+    public void initModel() {
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+    }
+
+}
