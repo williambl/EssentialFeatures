7b965a71b19b06009f37418854a5e972ec113f3e
williambl <william@williambl.com>
2016-08-11 22:22:21 +0100

    Added Iron-Fortified Cereal

--- src/main/java/net/anti_quark/EssentialFeatures/common/item/ItemCereal.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/item/ItemCereal.java
@@ -6,9 +6,11 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -16,23 +18,34 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemCereal extends ItemFood {
+	
+	boolean isIron;
 
-	public ItemCereal(String registryName, float saturation, int healAmount) {
+	public ItemCereal(String registryName, float saturation, int healAmount, boolean isIron) {
 		super(healAmount, saturation, false);
         this.setCreativeTab(CreativeTabs.FOOD);
+        this.isIron = isIron;
         this.setRegistryName(registryName);
         this.setUnlocalizedName(this.getRegistryName().toString());;
         GameRegistry.register(this);
+    }
+	
+    public boolean hasEffect(ItemStack stack)
+    {
+        return isIron;
     }
 	
     @Nullable
     public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityLivingBase entityLiving)
     {
         super.onItemUseFinish(stack, worldIn, entityLiving);
+        
+        if (isIron) {entityLiving.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 600, 1)); 
+        	entityLiving.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 200, 1));}
+        
         return new ItemStack(Items.BOWL);
     }
     
-    @SideOnly(Side.CLIENT)
     public void initModel() {
         ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
--- src/main/java/net/anti_quark/EssentialFeatures/common/item/ModItems.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/item/ModItems.java
@@ -5,14 +5,17 @@ import net.anti_quark.EssentialFeatures.common.block.ModBlocks;
 public class ModItems {
 	
 	public static ItemCereal itemCereal;
+	public static ItemCereal itemIronCereal;
 	
 	public static void addItems () 
 	{
-		itemCereal = new ItemCereal("cereal", 1, 6);
+		itemCereal = new ItemCereal("cereal", 1, 6, false);
+		itemIronCereal = new ItemCereal("iron_cereal", 3, 6, true);
 	}
 	
 	public static void initModels ()
 	{
 		itemCereal.initModel();
+		itemIronCereal.initModel();
 	}
 }
