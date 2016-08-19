37ff556984494dffe6f83ac8bf9728a574cc613c
williambl <william@williambl.com>
2016-08-19 21:59:07 +0100

    Changed Material of Block Breaker to PISTON from ROCK

--- src/main/java/net/anti_quark/EssentialFeatures/common/block/ModBlocks.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/block/ModBlocks.java
@@ -20,7 +20,7 @@ public class ModBlocks {
         litStainedLamp = new BlockStainedLamp("litstainedlamp", Material.GLASS, 0.3F, 1.5F, true);
         polishedGlowstone = new BlockPolishedGlowstone("polishedglowstone", Material.GLASS, 1, 2);
         snowBrick = new BlockSnowBrick("snowbrick", Material.CRAFTED_SNOW, 1, 1);
-        blockBreaker = new BlockBlockBreaker("blockbreaker", Material.ROCK, 5, 5);
+        blockBreaker = new BlockBlockBreaker("blockbreaker", Material.PISTON, 5, 5);
     }
 
 	public static void initModels() 
