b7e936e9e2708c197ca0f296b3ac171db8669057
williambl <william@williambl.com>
2016-08-20 14:09:07 +0100

    Changed to vanilla naming scheme
    e.g: SNOW_BRICK vs snowBrick

--- src/main/java/net/anti_quark/EssentialFeatures/common/block/ModBlocks.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/block/ModBlocks.java
@@ -4,36 +4,36 @@ import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 
-	public static BlockViewedBlock viewedBlock;
-	public static BlockSmoothGlowstone smoothGlowstone;
-	public static BlockPolishedGlowstone polishedGlowstone;
-	public static BlockStainedLamp stainedLamp;
-	public static BlockStainedLamp litStainedLamp;
-	public static BlockSnowBrick snowBrick;
-	public static BlockBlockBreaker blockBreaker;
+	public static BlockViewedBlock VIEWED_BLOCK;
+	public static BlockSmoothGlowstone SMOOTH_GLOWSTONE;
+	public static BlockPolishedGlowstone POLISHED_GLOWSTONE;
+	public static BlockStainedLamp STAINED_LAMP;
+	public static BlockStainedLamp LIT_STAINED_LAMP;
+	public static BlockSnowBrick SNOW_BRICK;
+	public static BlockBlockBreaker BLOCK_BREAKER;
 	public static BlockCryingObsidian CRYING_OBSIDIAN;
 	
 	public static void addBlocks() 
 	{
-        viewedBlock = new BlockViewedBlock("viewedblock", Material.ROCK, 5, 5);
-        smoothGlowstone = new BlockSmoothGlowstone("smoothglowstone", Material.GLASS, 1, 2);
-        stainedLamp = new BlockStainedLamp("stainedlamp", Material.GLASS, 0.3F, 1.5F, false);
-        litStainedLamp = new BlockStainedLamp("litstainedlamp", Material.GLASS, 0.3F, 1.5F, true);
-        polishedGlowstone = new BlockPolishedGlowstone("polishedglowstone", Material.GLASS, 1, 2);
-        snowBrick = new BlockSnowBrick("snowbrick", Material.CRAFTED_SNOW, 1, 1);
-        blockBreaker = new BlockBlockBreaker("blockbreaker", Material.PISTON, 5, 5);
+        VIEWED_BLOCK = new BlockViewedBlock("viewedblock", Material.ROCK, 5, 5);
+        SMOOTH_GLOWSTONE = new BlockSmoothGlowstone("smoothglowstone", Material.GLASS, 1, 2);
+        STAINED_LAMP = new BlockStainedLamp("stainedlamp", Material.GLASS, 0.3F, 1.5F, false);
+        LIT_STAINED_LAMP = new BlockStainedLamp("litstainedlamp", Material.GLASS, 0.3F, 1.5F, true);
+        POLISHED_GLOWSTONE = new BlockPolishedGlowstone("polishedglowstone", Material.GLASS, 1, 2);
+        SNOW_BRICK = new BlockSnowBrick("snowbrick", Material.CRAFTED_SNOW, 1, 1);
+        BLOCK_BREAKER = new BlockBlockBreaker("blockbreaker", Material.PISTON, 5, 5);
         CRYING_OBSIDIAN = new BlockCryingObsidian("crying_obsidian", Material.ROCK, 100, 100);
     }
 
 	public static void initModels() 
 	{
-		viewedBlock.initModel();
-		smoothGlowstone.initModel();
-		stainedLamp.initModel();
-		litStainedLamp.initModel();
-		polishedGlowstone.initModel();
-		snowBrick.initModel();
-		blockBreaker.initModel();
+		VIEWED_BLOCK.initModel();
+		SMOOTH_GLOWSTONE.initModel();
+		STAINED_LAMP.initModel();
+		LIT_STAINED_LAMP.initModel();
+		POLISHED_GLOWSTONE.initModel();
+		SNOW_BRICK.initModel();
+		BLOCK_BREAKER.initModel();
 		CRYING_OBSIDIAN.initModel();
 	}
 }
