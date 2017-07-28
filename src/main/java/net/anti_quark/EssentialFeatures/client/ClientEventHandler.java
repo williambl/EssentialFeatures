592d088bfd24880cabe3eb4b29e181b70e2e1702
williambl <william@williambl.com>
2017-07-28 09:51:00 +0100

    All music is now cancelled when it starts

--- src/main/java/net/anti_quark/EssentialFeatures/client/ClientEventHandler.java
+++ src/main/java/net/anti_quark/EssentialFeatures/client/ClientEventHandler.java
@@ -3,6 +3,8 @@ package net.anti_quark.EssentialFeatures.client;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.SoundManager;
+import net.minecraft.util.SoundCategory;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.client.event.sound.SoundEvent;
 import net.minecraftforge.client.event.sound.SoundEvent.SoundSourceEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -10,11 +12,12 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ClientEventHandler {
 	
 	@SubscribeEvent
-	public void playSoundEvent (SoundSourceEvent event) {
-	    if (event.getSound().getSound().getSoundLocation().toString().substring(0, 15) == "minecraft:music") {
-	    	Minecraft.getMinecraft().getSoundHandler().stopSound(event.getSound());
-	    	//event.getManager().playSound(p_sound);
+	public void playSoundEvent (PlaySoundEvent event) {
+	    if (event.getSound().getCategory() == SoundCategory.MUSIC) {
+	    		event.setResultSound(null);
+	    		//event.getManager().playSound(p_sound);
 	    }
+	    System.out.println(event.getName());
 	}
 
 }
