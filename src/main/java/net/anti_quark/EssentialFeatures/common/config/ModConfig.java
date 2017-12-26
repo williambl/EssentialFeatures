b0dc72026e545b0097618c18a2c798112ebd32e4
williambl <william@williambl.com>
2017-12-26 12:29:17 +0000

    Added a config class with example variable

---
+++ src/main/java/net/anti_quark/EssentialFeatures/common/config/ModConfig.java
@@ -0,0 +1,32 @@
+package net.anti_quark.EssentialFeatures.common.config;
+
+import net.anti_quark.EssentialFeatures.EssentialFeatures;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Config(modid = EssentialFeatures.MODID)
+public class ModConfig {
+
+    @Config.Comment("This is an example boolean property.")
+    public static boolean fooBar = false;
+
+
+    @Mod.EventBusSubscriber(modid = EssentialFeatures.MODID)
+    private static class EventHandler {
+
+        /**
+         * Inject the new values and save to the config file when the config has been changed from the GUI.
+         *
+         * @param event The event
+         */
+        @SubscribeEvent
+        public static void onConfigChanged(final ConfigChangedEvent.OnConfigChangedEvent event) {
+            if (event.getModID().equals(EssentialFeatures.MODID)) {
+                ConfigManager.sync(EssentialFeatures.MODID, Config.Type.INSTANCE);
+            }
+        }
+    }
+}
