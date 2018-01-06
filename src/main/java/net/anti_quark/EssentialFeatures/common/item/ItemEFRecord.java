7b83f392a4c1467e784b90cc19a490872db9a0e9
williambl <william@williambl.com>
2018-01-06 23:58:36 +0000

    Added some debug code to try and work out why the records won't play

--- src/main/java/net/anti_quark/EssentialFeatures/common/item/ItemEFRecord.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/item/ItemEFRecord.java
@@ -38,14 +38,54 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.input.Mouse;
 
 public class ItemEFRecord extends ItemRecord
 {
+
+    private static final Map<SoundEvent, ItemRecord> RECORDS = Maps.<SoundEvent, ItemRecord>newHashMap();
+    private final SoundEvent sound;
+    private final String displayName;
+
 	public ItemEFRecord(String recordName, SoundEvent soundIn)
     {
-    		super(recordName, soundIn);
+        super(recordName, soundIn);
+        System.out.println("hello, constructing " + recordName);
+        this.displayName = "item.record." + recordName + ".desc";
+        this.sound = soundIn;
+        RECORDS.put(this.sound, this);
         this.setRegistryName(recordName);
         this.setUnlocalizedName(this.getRegistryName().toString());
+        this.maxStackSize = 1;
+        this.setCreativeTab(CreativeTabs.MISC);
+    }
+
+    /**
+     * Called when a Block is right-clicked with this Item
+     */
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        IBlockState iblockstate = worldIn.getBlockState(pos);
+
+        if (iblockstate.getBlock() == Blocks.JUKEBOX && !((Boolean)iblockstate.getValue(BlockJukebox.HAS_RECORD)).booleanValue())
+        {
+            if (!worldIn.isRemote)
+            {
+                System.out.println("Hello");
+                ItemStack itemstack = player.getHeldItem(hand);
+                ((BlockJukebox)Blocks.JUKEBOX).insertRecord(worldIn, pos, iblockstate, itemstack);
+                worldIn.playEvent((EntityPlayer)null, 1010, pos, Item.getIdFromItem(this));
+                itemstack.shrink(1);
+                player.addStat(StatList.RECORD_PLAYED);
+            }
+
+            return EnumActionResult.SUCCESS;
+        }
+        else
+        {
+            return EnumActionResult.PASS;
+        }
     }
 	
     public void initModel() {
