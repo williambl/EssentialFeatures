77522130842bcc709317d7d35064cbfecc47c097
williambl <william@williambl.com>
2018-01-08 22:06:41 +0000

    Made portable jukebox support getting SoundEvents from ItemRecords

--- src/main/java/net/anti_quark/EssentialFeatures/common/item/ItemPortableJukebox.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/item/ItemPortableJukebox.java
@@ -1,11 +1,10 @@
 package net.anti_quark.EssentialFeatures.common.item;
 
-import net.anti_quark.EssentialFeatures.client.music.ModSound;
 import net.anti_quark.EssentialFeatures.client.music.MovingSoundGeneric;
 import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemRecord;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -13,6 +12,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class ItemPortableJukebox extends EFItem {
+
+    public ItemRecord record;
 
     public ItemPortableJukebox (String registryName, CreativeTabs tab) {
         super(registryName, tab);
@@ -23,7 +24,7 @@ public class ItemPortableJukebox extends EFItem {
      */
     public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
     {
-        Minecraft.getMinecraft().getSoundHandler().playSound(new MovingSoundGeneric(player, ModSound.RECORD_SCARLET));
+        Minecraft.getMinecraft().getSoundHandler().playSound(new MovingSoundGeneric(player, record.getSound()));
         return EnumActionResult.SUCCESS;
     }
 }
