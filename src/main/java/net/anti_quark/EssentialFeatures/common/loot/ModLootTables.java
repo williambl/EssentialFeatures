4b7c81d83a06a2273cdb9b2f35b5a024d722c95e
williambl <william@williambl.com>
2018-02-12 21:22:40 +0000

    Portable jukeboxes are now correctly added as loot

--- src/main/java/net/anti_quark/EssentialFeatures/common/loot/ModLootTables.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/loot/ModLootTables.java
@@ -8,6 +8,7 @@ import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraft.world.storage.loot.functions.LootFunction;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ModLootTables {
@@ -15,17 +16,19 @@ public class ModLootTables {
     public static LootEntry entry = new LootEntryItem(
             ModItems.PORTABLE_JUKEBOX, 100, 50, new LootFunction[0], new LootCondition[0], "essentialfeatures:loot_portable_jukebox");
 
-    @SubscribeEvent
-    public void onLootTableLoad(final LootTableLoadEvent event) {
-        ResourceLocation name = event.getName();
-        if(name.equals(LootTableList.CHESTS_DESERT_PYRAMID)
-                || name.equals(LootTableList.CHESTS_ABANDONED_MINESHAFT)
-                || name.equals(LootTableList.CHESTS_JUNGLE_TEMPLE)
-                || name.equals(LootTableList.CHESTS_SIMPLE_DUNGEON)
-                || name.equals(LootTableList.CHESTS_NETHER_BRIDGE)
-                ) {
-            System.out.println("injecting loot table");
-            event.getTable().getPool("main").addEntry(entry);
+    @Mod.EventBusSubscriber
+    public static class RegistrationHandler {
+        @SubscribeEvent
+        public static void onLootTableLoad(LootTableLoadEvent event) {
+            ResourceLocation name = event.getName();
+            if (name.equals(LootTableList.CHESTS_DESERT_PYRAMID)
+                    || name.equals(LootTableList.CHESTS_ABANDONED_MINESHAFT)
+                    || name.equals(LootTableList.CHESTS_JUNGLE_TEMPLE)
+                    || name.equals(LootTableList.CHESTS_SIMPLE_DUNGEON)
+                    || name.equals(LootTableList.CHESTS_NETHER_BRIDGE)
+                    ) {
+                event.getTable().getPool("main").addEntry(entry);
+            }
         }
     }
 }
