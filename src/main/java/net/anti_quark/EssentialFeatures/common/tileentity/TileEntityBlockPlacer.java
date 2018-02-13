632a8c795575897bda7aacb184d295e9c326e9f7
williambl <william@williambl.com>
2018-02-13 21:42:26 +0000

    Added some more stuff from TileEntityDispenser to TileEntityBlockPlacer

--- src/main/java/net/anti_quark/EssentialFeatures/common/tileentity/TileEntityBlockPlacer.java
+++ src/main/java/net/anti_quark/EssentialFeatures/common/tileentity/TileEntityBlockPlacer.java
@@ -1,12 +1,19 @@
 package net.anti_quark.EssentialFeatures.common.tileentity;
 
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityDispenser;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.datafix.DataFixer;
 import net.minecraft.util.datafix.FixTypes;
 import net.minecraft.util.datafix.walkers.ItemStackDataLists;
 
+import java.util.Random;
+
 public class TileEntityBlockPlacer extends TileEntityDispenser
 {
+    private static final Random RNG = new Random();
+    private NonNullList<ItemStack> stacks = NonNullList.<ItemStack>withSize(9, ItemStack.EMPTY);
+
     public static void registerFixes(DataFixer fixer)
     {
         fixer.registerWalker(FixTypes.BLOCK_ENTITY, new ItemStackDataLists(TileEntityBlockPlacer.class, "Items"));
@@ -22,6 +29,6 @@ public class TileEntityBlockPlacer extends TileEntityDispenser
 
     public String getGuiID()
     {
-        return "minecraft:dropper";
+        return "minecraft:dispenser";
     }
 }
