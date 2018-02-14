7d1bf0f1b64dba8df9bccc1543f073f55cbcacad
williambl <william@williambl.com>
2018-02-14 16:40:12 +0000

    Added more metadata

--- src/main/java/com/williambl/essentialfeatures/EssentialFeatures.java
+++ src/main/java/com/williambl/essentialfeatures/EssentialFeatures.java
@@ -8,14 +8,15 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = EssentialFeatures.MODID, version = EssentialFeatures.VERSION)
+@Mod(modid = EssentialFeatures.MODID, version = EssentialFeatures.VERSION, name = EssentialFeatures.NAME)
 public class EssentialFeatures {
 	
 	@Mod.Instance("essentialfeatures")
 	public static EssentialFeatures instance;
 	
     public static final String MODID = "essentialfeatures";
-    public static final String VERSION = "1.2.1";
+    public static final String NAME = "Essential Features";
+    public static final String VERSION = "2.0.0";
     
     @SidedProxy(
     		clientSide="com.williambl.essentialfeatures.client.ClientProxy",
