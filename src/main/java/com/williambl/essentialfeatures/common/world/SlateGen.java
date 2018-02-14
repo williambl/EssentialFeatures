f9d67e4c92b3d2e645768e03ccd2ded44ca1ff0b
williambl <william@williambl.com>
2018-02-14 19:57:10 +0000

    Slate now generates with all 8 layers

--- src/main/java/com/williambl/essentialfeatures/common/world/SlateGen.java
+++ src/main/java/com/williambl/essentialfeatures/common/world/SlateGen.java
@@ -2,8 +2,8 @@ package com.williambl.essentialfeatures.common.world;
 
 import java.util.Random;
 
+import com.williambl.essentialfeatures.common.block.BlockSlate;
 import com.williambl.essentialfeatures.common.block.ModBlocks;
-import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -17,7 +17,7 @@ public class SlateGen implements IWorldGenerator {
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 		if (world.provider.getDimension() == 0)
-			generateOre(ModBlocks.SLATE.getDefaultState(), world, random, chunkX, chunkZ, 40, 80, 10, 25);
+			generateOre(ModBlocks.SLATE.getDefaultState().withProperty(BlockSlate.LAYERS, 8), world, random, chunkX, chunkZ, 40, 80, 10, 25);
 	}
 	
 	public void generateOre(IBlockState block, World world, Random random, int chunkX, int chunkZ, int minY, int maxY, int chancesToSpawn, int veinSize) {
