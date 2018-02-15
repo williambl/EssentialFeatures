6fb4ddd6c0698c17b65b205f89c3c18ddf79547f
williambl <william@williambl.com>
2018-02-15 16:58:39 +0000

    Added more particle effects to the Blaze Block

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockBlaze.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockBlaze.java
@@ -23,12 +23,11 @@ public class BlockBlaze extends EFBlock {
     @Override
     public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn)
     {
-        if (!entityIn.isImmuneToFire() && entityIn instanceof EntityLivingBase && !EnchantmentHelper.hasFrostWalkerEnchantment((EntityLivingBase)entityIn))
-        {
+        if (!entityIn.isImmuneToFire() && entityIn instanceof EntityLivingBase && !EnchantmentHelper.hasFrostWalkerEnchantment((EntityLivingBase)entityIn)) {
             entityIn.attackEntityFrom(DamageSource.HOT_FLOOR, 1.0F);
             entityIn.setFire(5);
         }
-
+        particleExplosion(worldIn, pos, worldIn.rand);
         super.onEntityWalk(worldIn, pos, entityIn);
     }
 
@@ -41,6 +40,21 @@ public class BlockBlaze extends EFBlock {
             double d2 = (double)pos.getZ() + rand.nextDouble();
 
             worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+        }
+        if (rand.nextDouble() < 0.1) {
+            particleExplosion(worldIn, pos, rand);
+        }
+    }
+
+    private void particleExplosion(World worldIn, BlockPos pos, Random rand) {
+        for (int i = 0; i < 5; ++i) {
+            double d0 = (double) pos.getX() + rand.nextDouble();
+            double d1 = (double) pos.getY() + rand.nextDouble() * 0.5D + 0.5D;
+            double d2 = (double) pos.getZ() + rand.nextDouble();
+
+            worldIn.spawnParticle(EnumParticleTypes.SMOKE_LARGE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            worldIn.spawnParticle(EnumParticleTypes.FLAME, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+            worldIn.spawnParticle(EnumParticleTypes.LAVA, d0, d1, d2, 0.0D, 0.0D, 0.0D);
         }
     }
 }
