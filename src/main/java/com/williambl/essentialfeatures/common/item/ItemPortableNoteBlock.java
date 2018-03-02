ae1bba68c1443cbd6495dcf15e9978fea55722d6
williambl <william@williambl.com>
2018-03-02 18:09:43 +0000

    The portable note block can now play a note

--- src/main/java/com/williambl/essentialfeatures/common/item/ItemPortableNoteBlock.java
+++ src/main/java/com/williambl/essentialfeatures/common/item/ItemPortableNoteBlock.java
@@ -1,16 +1,22 @@
 package com.williambl.essentialfeatures.common.item;
 
+import com.google.common.collect.Lists;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockNote;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.List;
+
 public class ItemPortableNoteBlock extends EFItem {
+
+    private static final List<SoundEvent> INSTRUMENTS = Lists.newArrayList(SoundEvents.BLOCK_NOTE_HARP, SoundEvents.BLOCK_NOTE_BASEDRUM, SoundEvents.BLOCK_NOTE_SNARE, SoundEvents.BLOCK_NOTE_HAT, SoundEvents.BLOCK_NOTE_BASS, SoundEvents.BLOCK_NOTE_FLUTE, SoundEvents.BLOCK_NOTE_BELL, SoundEvents.BLOCK_NOTE_GUITAR, SoundEvents.BLOCK_NOTE_CHIME, SoundEvents.BLOCK_NOTE_XYLOPHONE);
 
     public ItemPortableNoteBlock(String registryName) {
         super(registryName, CreativeTabs.TOOLS);
@@ -23,6 +29,13 @@ public class ItemPortableNoteBlock extends EFItem {
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+        worldIn.playSound((EntityPlayer)null, playerIn.getPosition(), getInstrument(0), SoundCategory.RECORDS, 3.0F, 1.0f);
         return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
+
+    private SoundEvent getInstrument(int eventId) {
+        if (eventId < 0 || eventId >= INSTRUMENTS.size())
+            eventId = 0;
+        return INSTRUMENTS.get(eventId);
     }
 }
