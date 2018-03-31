0829c1c4ac830cc37483e2491fe2c562987742bd
williambl <william@williambl.com>
2018-03-31 23:51:25 +0100

    Added all constructors for sharpened arrow

--- src/main/java/com/williambl/essentialfeatures/common/entity/EntitySharpenedArrow.java
+++ src/main/java/com/williambl/essentialfeatures/common/entity/EntitySharpenedArrow.java
@@ -1,5 +1,6 @@
 package com.williambl.essentialfeatures.common.entity;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -8,6 +9,15 @@ public class EntitySharpenedArrow extends EntityArrow {
 
     public EntitySharpenedArrow(World worldIn) {
         super(worldIn);
+    }
+
+    public EntitySharpenedArrow(World worldIn, double x, double y, double z)
+    {
+        super(worldIn, x, y, z);
+    }
+
+    public EntitySharpenedArrow(World worldIn, EntityLivingBase shooter) {
+        super(worldIn, shooter);
     }
 
     @Override
