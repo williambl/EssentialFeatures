5d360aa78fc9b083c19c600c76238f9e596c9859
williambl <william@williambl.com>
2018-05-16 22:26:42 +0100

    Added a thanks for installing message on player join

--- src/main/java/com/williambl/essentialfeatures/client/ClientEventHandler.java
+++ src/main/java/com/williambl/essentialfeatures/client/ClientEventHandler.java
@@ -1,10 +1,13 @@
 package com.williambl.essentialfeatures.client;
 
+import com.williambl.essentialfeatures.EssentialFeatures;
 import com.williambl.essentialfeatures.client.music.CustomMusic;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class ClientEventHandler {
 	
@@ -14,6 +17,11 @@ public class ClientEventHandler {
 	    	    ISound result = CustomMusic.PlayMusic(event.getSound());
 	    	    event.setResultSound(result);
 	    }
+	}
+
+	@SubscribeEvent
+	public void playerLoginEvent (PlayerEvent.PlayerLoggedInEvent event) {
+		event.player.sendMessage(new TextComponentString("Thank you for installing Essential Features v" + EssentialFeatures.VERSION + "!"));
 	}
 
 }
