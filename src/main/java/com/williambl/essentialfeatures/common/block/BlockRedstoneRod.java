96b0f6656d86be5ec08f28da898cd2d8de673a80
williambl <william@williambl.com>
2019-02-22 17:12:05 +0000

    Make Redstone Rod directional like End Rod

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockRedstoneRod.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockRedstoneRod.java
@@ -5,27 +5,36 @@ import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.util.*;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class BlockRedstoneRod extends EFBlock implements ITileEntityProvider {
 
     public static final PropertyBool POWERED = PropertyBool.create("powered");
+    public static final PropertyDirection FACING = PropertyDirection.create("facing");
+
+    protected static final AxisAlignedBB END_ROD_VERTICAL_AABB = new AxisAlignedBB(0.375D, 0.0D, 0.375D, 0.625D, 1.0D, 0.625D);
+    protected static final AxisAlignedBB END_ROD_NS_AABB = new AxisAlignedBB(0.375D, 0.375D, 0.0D, 0.625D, 0.625D, 1.0D);
+    protected static final AxisAlignedBB END_ROD_EW_AABB = new AxisAlignedBB(0.0D, 0.375D, 0.375D, 1.0D, 0.625D, 0.625D);
 
     public BlockRedstoneRod(String registryName, Material material, CreativeTabs tab, SoundType soundType, float hardness, float resistance) {
         super(registryName, material, tab, soundType, hardness, resistance);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(POWERED, Boolean.FALSE));
+        this.setDefaultState(this.blockState.getBaseState().withProperty(POWERED, Boolean.FALSE).withProperty(FACING, EnumFacing.UP));
         this.hasTileEntity = true;
     }
-
 
     @Nullable
     @Override
@@ -37,6 +46,91 @@ public class BlockRedstoneRod extends EFBlock implements ITileEntityProvider {
     public void breakBlock(World world, BlockPos pos, IBlockState state) {
         super.breakBlock(world, pos, state);
         world.removeTileEntity(pos);
+    }
+
+    /**
+     * Returns the blockstate with the given rotation from the passed blockstate. If inapplicable, returns the passed
+     * blockstate.
+     */
+    public IBlockState withRotation(IBlockState state, Rotation rot)
+    {
+        return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
+    }
+
+    /**
+     * Returns the blockstate with the given mirror of the passed blockstate. If inapplicable, returns the passed
+     * blockstate.
+     */
+    public IBlockState withMirror(IBlockState state, Mirror mirrorIn)
+    {
+        return state.withProperty(FACING, mirrorIn.mirror((EnumFacing)state.getValue(FACING)));
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        switch (((EnumFacing)state.getValue(FACING)).getAxis())
+        {
+            case X:
+            default:
+                return END_ROD_EW_AABB;
+            case Z:
+                return END_ROD_NS_AABB;
+            case Y:
+                return END_ROD_VERTICAL_AABB;
+        }
+    }
+
+    /**
+     * Used to determine ambient occlusion and culling when rebuilding chunks for render
+     */
+    @Override
+    public boolean isOpaqueCube(IBlockState state)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
+    }
+
+    /**
+     * Called by ItemBlocks just before a block is actually set in the world, to allow for adjustments to the
+     * IBlockstate
+     */
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer, EnumHand hand)
+    {
+        IBlockState iblockstate = world.getBlockState(pos.offset(facing.getOpposite()));
+
+        if (iblockstate.getBlock() == ModBlocks.REDSTONE_ROD)
+        {
+            EnumFacing enumfacing = (EnumFacing)iblockstate.getValue(FACING);
+
+            if (enumfacing == facing)
+            {
+                return this.getDefaultState().withProperty(FACING, facing.getOpposite());
+            }
+        }
+
+        return this.getDefaultState().withProperty(FACING, facing);
+    }
+
+    @Override
+    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
+    {
+        EnumFacing enumfacing = (EnumFacing)stateIn.getValue(FACING);
+        double d0 = (double)pos.getX() + 0.55D - (double)(rand.nextFloat() * 0.1F);
+        double d1 = (double)pos.getY() + 0.55D - (double)(rand.nextFloat() * 0.1F);
+        double d2 = (double)pos.getZ() + 0.55D - (double)(rand.nextFloat() * 0.1F);
+        double d3 = (double)(0.4F - (rand.nextFloat() + rand.nextFloat()) * 0.4F);
+
+        if (rand.nextInt(5) == 0)
+        {
+            worldIn.spawnParticle(EnumParticleTypes.END_ROD, d0 + (double)enumfacing.getFrontOffsetX() * d3, d1 + (double)enumfacing.getFrontOffsetY() * d3, d2 + (double)enumfacing.getFrontOffsetZ() * d3, rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D);
+        }
     }
 
     @Override
@@ -51,17 +145,40 @@ public class BlockRedstoneRod extends EFBlock implements ITileEntityProvider {
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, POWERED);
+        return new BlockStateContainer(this, POWERED, FACING);
     }
 
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(POWERED, (meta & 1) > 0);
+    public IBlockState getStateFromMeta(int meta)
+    {
+        IBlockState iblockstate = this.getDefaultState();
+
+        if (meta > 5) {
+            meta -= 6;
+            iblockstate = iblockstate.withProperty(POWERED, true);
+        }
+
+        iblockstate = iblockstate.withProperty(FACING, EnumFacing.getFront(meta));
+        return iblockstate;
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
-        return state.getValue(POWERED) ? 1 : 0;
+        int meta = state.getValue(FACING).getIndex();
+        if (state.getValue(POWERED))
+            meta += 6;
+        return meta;
+    }
+
+    @Override
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face)
+    {
+        return BlockFaceShape.UNDEFINED;
     }
 
     public void activate(World worldIn, BlockPos pos, IBlockState blockstate) {
