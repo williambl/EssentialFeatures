11658c7113fda360729c07d25cdab8ac12356144
williambl <william@williambl.com>
2019-03-03 16:27:31 +0000

    Make doors drop correct item.
    Fixes #51.

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockEFDoor.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockEFDoor.java
@@ -2,10 +2,14 @@ package com.williambl.essentialfeatures.common.block;
 
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
+
+import java.util.Random;
 
 public class BlockEFDoor extends BlockDoor {
 
@@ -27,6 +31,11 @@ public class BlockEFDoor extends BlockDoor {
 
     private int getOpenSound() {
         return this.openSound;
+    }
+
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return state.getValue(HALF) == BlockDoor.EnumDoorHalf.UPPER ? Items.AIR : Item.getItemFromBlock(this);
     }
 
     public void initModel() {
