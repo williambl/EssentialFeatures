c5f39053748cc853a0539850dc07e59db96ce42d
williambl <william@williambl.com>
2019-03-16 17:39:04 +0000

    Reimplement many Redstone Torch methods in Stained Redstone Torch

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockStainedRedstoneTorch.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockStainedRedstoneTorch.java
@@ -1,12 +1,23 @@
 package com.williambl.essentialfeatures.common.block;
 
+import com.google.common.collect.Lists;
 import net.minecraft.block.BlockRedstoneTorch;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class BlockStainedRedstoneTorch extends BlockRedstoneTorch {
@@ -14,6 +25,7 @@ public class BlockStainedRedstoneTorch extends BlockRedstoneTorch {
     static final String[] names = new String[]{"white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "silver", "cyan", "purple", "blue", "brown", "green", "red", "black"};
     public final boolean isOn;
     public int colour;
+    private static final Map<World, List<Toggle>> toggles = new java.util.WeakHashMap<World, List<Toggle>>(); // FORGE - fix vanilla MC-101233
 
     public BlockStainedRedstoneTorch(String registryName, boolean isOn, int colour) {
         super(isOn);
@@ -34,5 +46,100 @@ public class BlockStainedRedstoneTorch extends BlockRedstoneTorch {
     public Item getItemDropped(IBlockState state, Random rand, int fortune)
     {
         return Item.getItemFromBlock(ModBlocks.STAINED_REDSTONE_TORCHES[colour]);
+    }
+
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+        boolean flag = this.shouldBeOff(worldIn, pos, state);
+        List<Toggle> list = (List)toggles.get(worldIn);
+
+        while (list != null && !list.isEmpty() && worldIn.getTotalWorldTime() - (list.get(0)).time > 60L)
+        {
+            list.remove(0);
+        }
+
+        if (this.isOn)
+        {
+            if (flag)
+            {
+                worldIn.setBlockState(pos, ModBlocks.STAINED_REDSTONE_TORCHES[colour+16].getDefaultState().withProperty(FACING, state.getValue(FACING)), 3);
+
+                if (this.isBurnedOut(worldIn, pos, true))
+                {
+                    worldIn.playSound((EntityPlayer)null, pos, SoundEvents.BLOCK_REDSTONE_TORCH_BURNOUT, SoundCategory.BLOCKS, 0.5F, 2.6F + (worldIn.rand.nextFloat() - worldIn.rand.nextFloat()) * 0.8F);
+
+                    for (int i = 0; i < 5; ++i)
+                    {
+                        double d0 = (double)pos.getX() + rand.nextDouble() * 0.6D + 0.2D;
+                        double d1 = (double)pos.getY() + rand.nextDouble() * 0.6D + 0.2D;
+                        double d2 = (double)pos.getZ() + rand.nextDouble() * 0.6D + 0.2D;
+                        worldIn.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+                    }
+
+                    worldIn.scheduleUpdate(pos, worldIn.getBlockState(pos).getBlock(), 160);
+                }
+            }
+        }
+        else if (!flag && !this.isBurnedOut(worldIn, pos, false))
+        {
+            worldIn.setBlockState(pos, ModBlocks.STAINED_REDSTONE_TORCHES[colour].getDefaultState().withProperty(FACING, state.getValue(FACING)), 3);
+        }
+    }
+
+    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
+    {
+        return new ItemStack(Item.getItemFromBlock(ModBlocks.STAINED_REDSTONE_TORCHES[colour]));
+    }
+
+    private boolean shouldBeOff(World worldIn, BlockPos pos, IBlockState state)
+    {
+        EnumFacing enumfacing = ((EnumFacing)state.getValue(FACING)).getOpposite();
+        return worldIn.isSidePowered(pos.offset(enumfacing), enumfacing);
+    }
+
+    private boolean isBurnedOut(World worldIn, BlockPos pos, boolean turnOff)
+    {
+        if (!toggles.containsKey(worldIn))
+        {
+            toggles.put(worldIn, Lists.newArrayList());
+        }
+
+        List<Toggle> list = (List)toggles.get(worldIn);
+
+        if (turnOff)
+        {
+            list.add(new Toggle(pos, worldIn.getTotalWorldTime()));
+        }
+
+        int i = 0;
+
+        for (int j = 0; j < list.size(); ++j)
+        {
+            Toggle blockredstonetorch$toggle = list.get(j);
+
+            if (blockredstonetorch$toggle.pos.equals(pos))
+            {
+                ++i;
+
+                if (i >= 8)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    static class Toggle
+    {
+        BlockPos pos;
+        long time;
+
+        public Toggle(BlockPos pos, long time)
+        {
+            this.pos = pos;
+            this.time = time;
+        }
     }
 }
