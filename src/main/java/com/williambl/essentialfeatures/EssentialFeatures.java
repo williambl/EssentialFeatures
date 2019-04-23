28af82c0790ce404799de959b3534f69e49f5437
williambl <william@williambl.com>
2019-04-23 19:55:01 +0100

    Update EssentialFeatures.java and remove proxies for 1.13

--- src/main/java/com/williambl/essentialfeatures/EssentialFeatures.java
+++ src/main/java/com/williambl/essentialfeatures/EssentialFeatures.java
@@ -1,41 +1,49 @@
 package com.williambl.essentialfeatures;
 
-import com.williambl.essentialfeatures.common.CommonProxy;
+import com.williambl.essentialfeatures.client.ClientEventHandler;
+import com.williambl.essentialfeatures.client.music.ModSound;
+import com.williambl.essentialfeatures.common.CommonEventHandler;
+import com.williambl.essentialfeatures.common.block.ModBlocks;
+import com.williambl.essentialfeatures.common.config.ModConfig;
+import com.williambl.essentialfeatures.common.craft.ModRecipes;
+import com.williambl.essentialfeatures.common.entity.ModEntities;
+import com.williambl.essentialfeatures.common.item.ModItems;
+import com.williambl.essentialfeatures.common.world.ModWorld;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
-@Mod(modid = EssentialFeatures.MODID, version = EssentialFeatures.VERSION, name = EssentialFeatures.NAME)
+@Mod(EssentialFeatures.MODID)
+@Mod.EventBusSubscriber(modid = EssentialFeatures.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EssentialFeatures {
 
-    @Mod.Instance("essentialfeatures")
-    public static EssentialFeatures instance;
-
     public static final String MODID = "essentialfeatures";
-    public static final String NAME = "Essential Features";
-    public static final String VERSION = "2.2.0";
 
-    @SidedProxy(
-            clientSide = "com.williambl.essentialfeatures.client.ClientProxy",
-            serverSide = "com.williambl.essentialfeatures.server.ServerProxy")
-    public static CommonProxy proxy;
+    @SubscribeEvent
+    public void setup(final FMLCommonSetupEvent event) {
+        ModSound.addSounds();
+        if (ModConfig.blocks)
+            ModBlocks.addBlocks();
+        if (ModConfig.items)
+            ModItems.addItems();
+        ModEntities.addEntities();
+        ModWorld.registerWorldGenerators();
+        if (ModConfig.smelting)
+            ModRecipes.addRecipes();
+        ModEntities.initRenderers();
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit();
+        CommonEventHandler handler = new CommonEventHandler();
+        MinecraftForge.EVENT_BUS.register(handler);
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        proxy.init();
-    }
-
-    @EventHandler
-    public void Postinit(FMLPostInitializationEvent event) {
-        proxy.postInit();
+    @SubscribeEvent
+    public void clientSetup(final FMLClientSetupEvent event) {
+        ClientEventHandler handler = new ClientEventHandler();
+        MinecraftForge.EVENT_BUS.register(handler);
+        ModBlocks.RegistrationHandler.registerBlockColors();
+        ModItems.RegistrationHandler.registerItemColors();
     }
 
 }
