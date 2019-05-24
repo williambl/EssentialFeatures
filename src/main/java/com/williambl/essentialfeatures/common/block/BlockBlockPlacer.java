0b1fbac3215d3313ea3d7eef3da82c2ccae3868e
williambl <william@williambl.com>
2019-05-24 22:34:22 +0100

    Update Block Placer

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockBlockPlacer.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockBlockPlacer.java
@@ -5,31 +5,27 @@ import net.minecraft.block.BlockDispenser;
 import net.minecraft.dispenser.BlockSourceImpl;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.dispenser.IBehaviorDispenseItem;
 import net.minecraft.dispenser.IBlockSource;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityDispenser;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-
-import java.util.Collection;
 
 public class BlockBlockPlacer extends BlockDispenser {
 
     private final IBehaviorDispenseItem dropBehavior = new BehaviorPlaceBlock();
 
     protected BlockBlockPlacer(String registryName, Material material, float hardness, float resistance) {
-        super();
-        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(TRIGGERED, Boolean.FALSE));
-        this.setHardness(hardness);
-        this.setResistance(resistance);
+        super(Properties.create(material).hardnessAndResistance(hardness, resistance));
+        this.setDefaultState(this.getStateContainer().getBaseState().with(FACING, EnumFacing.NORTH).with(TRIGGERED, Boolean.FALSE));
         this.setRegistryName(registryName);
     }
 
@@ -37,7 +33,7 @@ public class BlockBlockPlacer extends BlockDispenser {
      * Returns a new instance of a block's tile entity class. Called on placing the block.
      */
     @Override
-    public TileEntity createNewTileEntity(World worldIn, int meta) {
+    public TileEntity createNewTileEntity(IBlockReader worldIn) {
         return new TileEntityDispenser();
     }
 
@@ -46,21 +42,19 @@ public class BlockBlockPlacer extends BlockDispenser {
         BlockSourceImpl blocksourceimpl = new BlockSourceImpl(worldIn, pos);
         TileEntityDispenser tileentityblockplacer = blocksourceimpl.getBlockTileEntity();
 
-        if (tileentityblockplacer != null) {
-            int i = tileentityblockplacer.getDispenseSlot();
+        int i = tileentityblockplacer.getDispenseSlot();
 
-            if (i < 0) {
-                worldIn.playEvent(1001, pos, 0);
-            } else {
-                ItemStack itemstack = tileentityblockplacer.getStackInSlot(i);
+        if (i < 0) {
+            worldIn.playEvent(1001, pos, 0);
+        } else {
+            ItemStack itemstack = tileentityblockplacer.getStackInSlot(i);
 
-                if (!itemstack.isEmpty()) {
-                    ItemStack itemstack1;
+            if (!itemstack.isEmpty()) {
+                ItemStack itemstack1;
 
-                    itemstack1 = this.dropBehavior.dispense(blocksourceimpl, itemstack);
+                itemstack1 = this.dropBehavior.dispense(blocksourceimpl, itemstack);
 
-                    tileentityblockplacer.setInventorySlotContents(i, itemstack1);
-                }
+                tileentityblockplacer.setInventorySlotContents(i, itemstack1);
             }
         }
     }
@@ -73,21 +67,21 @@ public class BlockBlockPlacer extends BlockDispenser {
             if (block == Blocks.AIR)
                 return stack;
 
-            EnumFacing facing = source.getBlockState().getValue(BlockDispenser.FACING);
+            EnumFacing facing = source.getBlockState().get(BlockDispenser.FACING);
             EnumFacing.Axis axis = facing.getAxis();
 
             BlockPos pos = source.getBlockPos().offset(facing);
             World world = source.getWorld();
 
-            if (world.isAirBlock(pos) && block.canPlaceBlockAt(world, pos)) {
-                int meta = stack.getItem().getMetadata(stack.getItemDamage());
-                IBlockState state = block.getStateFromMeta(meta);
-                Collection<IProperty<?>> props = state.getPropertyKeys();
+            if (world.isAirBlock(pos)) {
+                IBlockState state = block.getStateForPlacement(new BlockItemUseContext(world, null, stack, pos, facing, pos.getX(), pos.getY(), pos.getZ()));
 
-                world.setBlockState(pos, state);
-                SoundType soundtype = block.getSoundType();
-                world.playSound(null, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
-                stack.shrink(1);
+                if (state != null) {
+                    world.setBlockState(pos, state);
+                    SoundType soundtype = block.getSoundType();
+                    world.playSound(null, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
+                    stack.shrink(1);
+                }
             }
 
             return stack;
