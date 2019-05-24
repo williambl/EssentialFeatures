9908c631ccb0f22078db1ad2ff289cbd258254aa
williambl <william@williambl.com>
2019-05-24 22:48:50 +0100

    Update Fake Block Though I don't think it will be used any more

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockFake.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockFake.java
@@ -3,12 +3,8 @@ package com.williambl.essentialfeatures.common.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class BlockFake extends Block {
 
@@ -16,13 +12,13 @@ public class BlockFake extends Block {
 
     //This can be used if you want to place an unplaceable block
     public BlockFake(String registryName, ItemGroup tab, IBlockState realBlockState) {
-        super(Material.AIR);
+        super(Properties.create(Material.AIR));
         this.setRegistryName(registryName);
         this.realBlockState = realBlockState;
     }
 
     @Override
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+    public IBlockState getStateForPlacement(BlockItemUseContext context) {
         return realBlockState;
     }
 }
