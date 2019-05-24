c0447ec5186ac07c1ac853eb75de48f6fae67013
williambl <william@williambl.com>
2019-05-24 23:57:49 +0100

    Update Stained Lamp

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockStainedLamp.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockStainedLamp.java
@@ -1,96 +1,72 @@
 package com.williambl.essentialfeatures.common.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRedstoneLamp;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyEnum;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.EnumDyeColor;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 public class BlockStainedLamp extends Block {
 
-    public static final PropertyEnum<EnumDyeColor> COLOR = PropertyEnum.create("color", EnumDyeColor.class);
-    private final boolean isOn;
+    public static final BooleanProperty LIT = BlockRedstoneLamp.LIT;
     static final String[] names = new String[]{"white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "silver", "cyan", "purple", "blue", "brown", "green", "red", "black"};
 
     public BlockStainedLamp(String registryName, Material material, float hardness, float resistance, boolean isOn) {
-        super(material);
-        this.setHardness(hardness);
-        this.setResistance(resistance);
+        super(Properties.create(material).hardnessAndResistance(hardness, resistance).sound(SoundType.GLASS));
         this.setRegistryName(registryName);
-        this.setSoundType(SoundType.GLASS);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(COLOR, EnumDyeColor.WHITE));
-        this.isOn = isOn;
-        if (isOn) {
-            this.setLightLevel(1.0F);
-            this.setCreativeTab(null);
-        }
+        this.setDefaultState(this.getStateContainer().getBaseState().with(LIT, false));
     }
 
     @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, COLOR);
+    @SuppressWarnings("deprecation")
+    public int getLightValue(IBlockState state) {
+        return state.get(LIT) ? super.getLightValue(state) : 0;
     }
 
     @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(COLOR, EnumDyeColor.byMetadata(meta));
+    protected void fillStateContainer(StateContainer.Builder<Block, IBlockState> builder) {
+        builder.add(LIT);
+    }
+
+    @Nullable
+    @Override
+    public IBlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(LIT, context.getWorld().isBlockPowered(context.getPos()));
     }
 
     @Override
-    public int getMetaFromState(IBlockState state) {
-        return state.getValue(COLOR).getMetadata();
-    }
-
-    @Override
-    public void getSubBlocks(ItemGroup tab, NonNullList<ItemStack> list) {
-        for (EnumDyeColor enumdyecolor : EnumDyeColor.values()) {
-            list.add(new ItemStack(this, 1, enumdyecolor.getMetadata()));
-        }
-    }
-
-    @Override
-    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
-        if (!worldIn.isRemote) {
-            if (this.isOn && !worldIn.isBlockPowered(pos)) {
-                worldIn.setBlockState(pos, ModBlocks.LIT_STAINED_LAMP.getDefaultState().withProperty(COLOR, state.getValue(COLOR)), 2);
-            } else if (!this.isOn && worldIn.isBlockPowered(pos)) {
-                worldIn.setBlockState(pos, ModBlocks.LIT_STAINED_LAMP.getDefaultState().withProperty(COLOR, state.getValue(COLOR)), 2);
-            }
-        }
-    }
-
-    @Override
+    @SuppressWarnings("deprecation")
     public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
         if (!worldIn.isRemote) {
-            if (this.isOn && !worldIn.isBlockPowered(pos)) {
-                worldIn.scheduleUpdate(pos, this, 4);
-            } else if (!this.isOn && worldIn.isBlockPowered(pos)) {
-                worldIn.setBlockState(pos, ModBlocks.LIT_STAINED_LAMP.getDefaultState().withProperty(COLOR, state.getValue(COLOR)), 2);
+            boolean flag = state.get(LIT);
+            if (flag != worldIn.isBlockPowered(pos)) {
+                if (flag) {
+                    worldIn.getPendingBlockTicks().scheduleTick(pos, this, 4);
+                } else {
+                    worldIn.setBlockState(pos, state.cycle(LIT), 2);
+                }
             }
+
         }
     }
 
     @Override
-    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+    @SuppressWarnings("deprecation")
+    public void tick(IBlockState state, World worldIn, BlockPos pos, Random random) {
         if (!worldIn.isRemote) {
-            if (this.isOn && !worldIn.isBlockPowered(pos)) {
-                worldIn.setBlockState(pos, ModBlocks.STAINED_LAMP.getDefaultState().withProperty(COLOR, state.getValue(COLOR)), 2);
+            if (state.get(LIT) && !worldIn.isBlockPowered(pos)) {
+                worldIn.setBlockState(pos, state.cycle(LIT), 2);
             }
+
         }
     }
 
-    @Override
-    public int damageDropped(IBlockState state) {
-        return state.getValue(COLOR).getMetadata();
-    }
 }
