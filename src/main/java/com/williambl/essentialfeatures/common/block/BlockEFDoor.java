9e151fd19788fee342149d9dc9b84273e6b0e12c
williambl <william@williambl.com>
2019-05-24 22:47:21 +0100

    Update EFDoor

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockEFDoor.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockEFDoor.java
@@ -3,9 +3,9 @@ package com.williambl.essentialfeatures.common.block;
 import net.minecraft.block.BlockDoor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.state.properties.DoubleBlockHalf;
 
 import java.util.Random;
 
@@ -15,9 +15,8 @@ public class BlockEFDoor extends BlockDoor {
     private int openSound;
 
     public BlockEFDoor(String registryName, Material materialIn, float hardness, int closeSound, int openSound) {
-        super(materialIn);
+        super(Properties.create(materialIn).hardnessAndResistance(hardness));
         this.setRegistryName(registryName);
-        this.setHardness(hardness);
         this.closeSound = closeSound;
         this.openSound = openSound;
     }
@@ -30,9 +29,8 @@ public class BlockEFDoor extends BlockDoor {
         return this.openSound;
     }
 
-    public Item getItemDropped(IBlockState state, Random rand, int fortune)
-    {
-        return state.getValue(HALF) == BlockDoor.EnumDoorHalf.UPPER ? Items.AIR : Item.getItemFromBlock(this);
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return state.get(HALF) == DoubleBlockHalf.UPPER ? Items.AIR : Item.getItemFromBlock(this);
     }
 
 }
