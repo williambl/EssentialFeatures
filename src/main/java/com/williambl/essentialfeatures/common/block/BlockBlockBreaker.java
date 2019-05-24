d1acafde5d600523be2771a4faf8c17e46bfb55c
williambl <william@williambl.com>
2019-05-24 22:25:04 +0100

    Update Block Breaker

--- src/main/java/com/williambl/essentialfeatures/common/block/BlockBlockBreaker.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/BlockBlockBreaker.java
@@ -3,45 +3,40 @@ package com.williambl.essentialfeatures.common.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.PropertyBool;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
-import javax.annotation.Nullable;
-
 public class BlockBlockBreaker extends BlockDirectional {
 
-    public static final PropertyBool TRIGGERED = PropertyBool.create("triggered");
+    public static final BooleanProperty TRIGGERED = BooleanProperty.create("triggered");
 
     protected BlockBlockBreaker(String registryName, Material material, float hardness, float resistance) {
-        super(material);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(TRIGGERED, Boolean.FALSE));
-        this.setHardness(hardness);
-        this.setResistance(resistance);
+        super(Properties.create(material).hardnessAndResistance(hardness, resistance));
+        this.setDefaultState(this.getStateContainer().getBaseState().with(FACING, EnumFacing.NORTH).with(TRIGGERED, Boolean.FALSE));
         this.setRegistryName(registryName);
     }
 
     @Override
     public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
         if (!worldIn.isRemote) {
-            if (worldIn.isBlockPowered(pos) && state.getValue(TRIGGERED) == Boolean.valueOf(false)) {
+            if (worldIn.isBlockPowered(pos) && state.get(TRIGGERED) == Boolean.valueOf(false)) {
                 destroy(pos, state, worldIn);
-                worldIn.setBlockState(pos, state.cycleProperty(TRIGGERED), 6);
-            } else if (!worldIn.isBlockPowered(pos) && !state.getValue(TRIGGERED) == Boolean.FALSE) {
-                worldIn.setBlockState(pos, state.cycleProperty(TRIGGERED), 6);
+                worldIn.setBlockState(pos, state.cycle(TRIGGERED), 6);
+            } else if (!worldIn.isBlockPowered(pos) && !state.get(TRIGGERED) == Boolean.FALSE) {
+                worldIn.setBlockState(pos, state.cycle(TRIGGERED), 6);
             }
         }
     }
 
     public void destroy(BlockPos pos, IBlockState state, World worldIn) {
-        BlockPos offsetPos = pos.offset(state.getValue(FACING));
+        BlockPos offsetPos = pos.offset(state.get(FACING));
         Block offsetBlock = worldIn.getBlockState(offsetPos).getBlock();
 
         worldIn.destroyBlock(offsetPos, true);
@@ -50,7 +45,7 @@ public class BlockBlockBreaker extends BlockDirectional {
 
     @Override
     public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        worldIn.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
+        worldIn.setBlockState(pos, state.with(FACING, getFacingFromEntity(pos, placer)), 2);
     }
 
     public static EnumFacing getFacingFromEntity(BlockPos pos, EntityLivingBase entityIn) {
@@ -69,33 +64,9 @@ public class BlockBlockBreaker extends BlockDirectional {
         return entityIn.getHorizontalFacing().getOpposite();
     }
 
-
     @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, FACING, TRIGGERED);
-    }
-
-    @Override
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(FACING, getFacing(meta)).withProperty(TRIGGERED, (meta & 8) > 0);
-    }
-
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        int i = 0;
-        i = i | state.getValue(FACING).getIndex();
-
-        if (state.getValue(TRIGGERED)) {
-            i |= 8;
-        }
-
-        return i;
-    }
-
-    @Nullable
-    public static EnumFacing getFacing(int meta) {
-        int i = meta & 7;
-        return i > 5 ? null : EnumFacing.getFront(i);
+    protected void fillStateContainer(StateContainer.Builder<Block, IBlockState> builder) {
+        builder.add(FACING, TRIGGERED);
     }
 
 }
