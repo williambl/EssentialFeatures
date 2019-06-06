d323d735c69df23f00120512ebfb830ed06822e0
williambl <william@williambl.com>
2019-06-06 13:34:07 +0100

    Update ItemPortableNoteBlock Not sure why this wasn't caught the first time around...

--- src/main/java/com/williambl/essentialfeatures/common/item/ItemPortableNoteBlock.java
+++ src/main/java/com/williambl/essentialfeatures/common/item/ItemPortableNoteBlock.java
@@ -1,14 +1,13 @@
 package com.williambl.essentialfeatures.common.item;
 
 import com.google.common.collect.Lists;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
+import net.minecraft.init.Particles;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.state.properties.NoteBlockInstrument;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -17,16 +16,20 @@ import java.util.List;
 
 public class ItemPortableNoteBlock extends EFItem {
 
-    private static final List<SoundEvent> INSTRUMENTS = Lists.newArrayList(SoundEvents.BLOCK_NOTE_HARP, SoundEvents.BLOCK_NOTE_BASEDRUM, SoundEvents.BLOCK_NOTE_SNARE, SoundEvents.BLOCK_NOTE_HAT, SoundEvents.BLOCK_NOTE_BASS, SoundEvents.BLOCK_NOTE_FLUTE, SoundEvents.BLOCK_NOTE_BELL, SoundEvents.BLOCK_NOTE_GUITAR, SoundEvents.BLOCK_NOTE_CHIME, SoundEvents.BLOCK_NOTE_XYLOPHONE);
+    private static final List<SoundEvent> INSTRUMENTS = Lists.newArrayList(SoundEvents.BLOCK_NOTE_BLOCK_HARP, SoundEvents.BLOCK_NOTE_BLOCK_BASEDRUM, SoundEvents.BLOCK_NOTE_BLOCK_SNARE, SoundEvents.BLOCK_NOTE_BLOCK_HAT, SoundEvents.BLOCK_NOTE_BLOCK_BASS, SoundEvents.BLOCK_NOTE_BLOCK_FLUTE, SoundEvents.BLOCK_NOTE_BLOCK_BELL, SoundEvents.BLOCK_NOTE_BLOCK_GUITAR, SoundEvents.BLOCK_NOTE_BLOCK_CHIME, SoundEvents.BLOCK_NOTE_BLOCK_XYLOPHONE);
 
     public ItemPortableNoteBlock(String registryName) {
         super(registryName, ItemGroup.TOOLS);
     }
 
     @Override
-    public EnumActionResult onItemUse(EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        worldIn.playSound((EntityPlayer) null, playerIn.getPosition(), getInstrumentFromBlock(worldIn, pos), SoundCategory.RECORDS, 3.0F, getPitchFromPosition(pos));
-        worldIn.spawnParticle(EnumParticleTypes.NOTE, playerIn.posX, playerIn.posY + playerIn.getEyeHeight(), playerIn.posZ, 1.0F, 0F, 0F);
+    public EnumActionResult onItemUse(ItemUseContext context) {
+        World world = context.getWorld();
+        EntityPlayer player = context.getPlayer();
+        BlockPos pos = context.getPos();
+
+        world.playSound(null, player.getPosition(), getInstrumentFromBlock(world, pos), SoundCategory.RECORDS, 3.0F, getPitchFromPosition(pos));
+        world.spawnParticle(Particles.NOTE, player.posX, player.posY + player.getEyeHeight(), player.posZ, 1.0F, 0F, 0F);
         return EnumActionResult.SUCCESS;
     }
 
@@ -35,46 +38,13 @@ public class ItemPortableNoteBlock extends EFItem {
     }
 
     private SoundEvent getInstrumentFromBlock(World worldIn, BlockPos pos) {
-        IBlockState iblockstate = worldIn.getBlockState(pos);
-        Material material = iblockstate.getMaterial();
-        int i = 0;
-
-        if (material == Material.ROCK)
-            i = 1;
-
-        if (material == Material.SAND)
-            i = 2;
-
-        if (material == Material.GLASS)
-            i = 3;
-
-        if (material == Material.WOOD)
-            i = 4;
-
-        Block block = iblockstate.getBlock();
-
-        if (block == Blocks.CLAY)
-            i = 5;
-
-        if (block == Blocks.GOLD_BLOCK)
-            i = 6;
-
-        if (block == Blocks.WOOL)
-            i = 7;
-
-        if (block == Blocks.PACKED_ICE)
-            i = 8;
-
-        if (block == Blocks.BONE_BLOCK)
-            i = 9;
-
-        return getInstrument(i);
+        return NoteBlockInstrument.byState(worldIn.getBlockState(pos)).getSound();
     }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
-        worldIn.playSound((EntityPlayer) null, playerIn.getPosition(), getInstrument(0), SoundCategory.RECORDS, 3.0F, getPitchFromPosition(playerIn.getPosition()));
-        worldIn.spawnParticle(EnumParticleTypes.NOTE, playerIn.posX, playerIn.posY + playerIn.getEyeHeight(), playerIn.posZ, 1.0F, 0F, 0F);
+        worldIn.playSound(null, playerIn.getPosition(), getInstrument(0), SoundCategory.RECORDS, 3.0F, getPitchFromPosition(playerIn.getPosition()));
+        worldIn.spawnParticle(Particles.NOTE, playerIn.posX, playerIn.posY + playerIn.getEyeHeight(), playerIn.posZ, 1.0F, 0F, 0F);
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 
