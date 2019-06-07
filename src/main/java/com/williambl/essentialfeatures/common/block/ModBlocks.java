173cec7f0b422b073f6b704e714330cd4ac1c0d5
williambl <william@williambl.com>
2019-06-07 15:44:01 +0100

    Make ModBlocks use objectholders

--- src/main/java/com/williambl/essentialfeatures/common/block/ModBlocks.java
+++ src/main/java/com/williambl/essentialfeatures/common/block/ModBlocks.java
@@ -1,5 +1,6 @@
 package com.williambl.essentialfeatures.common.block;
 
+import com.williambl.essentialfeatures.EssentialFeatures;
 import com.williambl.essentialfeatures.common.item.ItemBlockDoor;
 import com.williambl.essentialfeatures.common.item.ItemSlate;
 import net.minecraft.block.Block;
@@ -17,84 +18,78 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.ObjectHolder;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@ObjectHolder(EssentialFeatures.MODID)
 public class ModBlocks {
 
+    @ObjectHolder("viewed_block")
     public static BlockViewedBlock VIEWED_BLOCK;
+    @ObjectHolder("smooth_glowstone")
     public static EFBlock SMOOTH_GLOWSTONE;
+    @ObjectHolder("polished_glowstone")
     public static EFBlock POLISHED_GLOWSTONE;
+    @ObjectHolder("snow_brick")
     public static EFBlock SNOW_BRICK;
+    @ObjectHolder("block_breaker")
     public static BlockBlockBreaker BLOCK_BREAKER;
+    @ObjectHolder("crying_obsidian")
     public static BlockCryingObsidian CRYING_OBSIDIAN;
+    @ObjectHolder("spike_block")
     public static BlockSpike SPIKE_BLOCK;
+    @ObjectHolder("block_placer")
     public static BlockBlockPlacer BLOCK_PLACER;
 
+    @ObjectHolder("carved_stone")
     public static EFBlock DECORATIVE_STONE;
+    @ObjectHolder("carved_andesite")
     public static EFBlock DECORATIVE_ANDESITE;
+    @ObjectHolder("carved_diorite")
     public static EFBlock DECORATIVE_DIORITE;
+    @ObjectHolder("carved_granite")
     public static EFBlock DECORATIVE_GRANITE;
 
+    @ObjectHolder("cream_bricks")
     public static EFBlock CREAM_BRICKS;
+    @ObjectHolder("dirty_bricks")
     public static EFBlock DIRTY_BRICKS;
+    @ObjectHolder("long_bricks")
     public static EFBlock LONG_BRICKS;
+    @ObjectHolder("blue_bricks")
     public static EFBlock BLUE_BRICKS;
+    @ObjectHolder("mixed_bricks")
     public static EFBlock MIXED_BRICKS;
 
+    @ObjectHolder("slate")
     public static BlockSlate SLATE;
+    @ObjectHolder("blaze_block")
     public static BlockBlaze BLAZE_BLOCK;
 
+    @ObjectHolder("packed_sand")
     public static EFBlock PACKED_SAND;
+    @ObjectHolder("packed_red_sand")
     public static EFBlock PACKED_RED_SAND;
+    @ObjectHolder("packed_gravel")
     public static EFBlock PACKED_GRAVEL;
 
+    @ObjectHolder("nettles")
     public static BlockNettles NETTLES;
 
+    @ObjectHolder("nether_brick_door")
     public static BlockEFDoor NETHER_BRICK_DOOR;
+    @ObjectHolder("purpur_door")
     public static BlockEFDoor PURPUR_DOOR;
 
+    @ObjectHolder("redstone_rod")
     public static BlockRedstoneRod REDSTONE_ROD;
 
     public static BlockStainedRedstoneTorch[] STAINED_REDSTONE_TORCHES;
     public static BlockStainedLamp[] STAINED_LAMPS;
 
     public static void addBlocks() {
-        VIEWED_BLOCK = new BlockViewedBlock("viewed_block", Material.ROCK, 5, 5);
-        SMOOTH_GLOWSTONE = new EFBlock("smooth_glowstone", Material.GLASS, SoundType.GLASS, 0.5f, 2, 1);
-        POLISHED_GLOWSTONE = new EFBlock("polished_glowstone", Material.GLASS, SoundType.GLASS, 1, 2, 1);
-        SNOW_BRICK = new EFBlock("snow_brick", Material.CRAFTED_SNOW, SoundType.SNOW, 0.5f, 1);
-        BLOCK_BREAKER = new BlockBlockBreaker("block_breaker", Material.PISTON, 3, 3);
-        CRYING_OBSIDIAN = new BlockCryingObsidian("crying_obsidian", Material.ROCK, 100, 100);
-        SPIKE_BLOCK = new BlockSpike("spike_block", Material.IRON, 1, 1);
-        BLOCK_PLACER = new BlockBlockPlacer("block_placer", Material.ROCK, 5, 5);
-
-        DECORATIVE_STONE = new EFBlock("decorative_stone", Material.ROCK, SoundType.STONE, 3, 3);
-        DECORATIVE_ANDESITE = new EFBlock("decorative_andesite", Material.ROCK, SoundType.STONE, 3, 3);
-        DECORATIVE_DIORITE = new EFBlock("decorative_diorite", Material.ROCK, SoundType.STONE, 3, 3);
-        DECORATIVE_GRANITE = new EFBlock("decorative_granite", Material.ROCK, SoundType.STONE, 3, 3);
-
-        CREAM_BRICKS = new EFBlock("cream_bricks", Material.ROCK, SoundType.STONE, 3, 3);
-        DIRTY_BRICKS = new EFBlock("dirty_bricks", Material.ROCK, SoundType.STONE, 3, 3);
-        LONG_BRICKS = new EFBlock("long_bricks", Material.ROCK, SoundType.STONE, 3, 3);
-        BLUE_BRICKS = new EFBlock("blue_bricks", Material.ROCK, SoundType.STONE, 3, 3);
-        MIXED_BRICKS = new EFBlock("mixed_bricks", Material.ROCK, SoundType.STONE, 3, 3);
-
-        SLATE = new BlockSlate("slate", Material.ROCK, 2, 3);
-        BLAZE_BLOCK = new BlockBlaze("blaze_block");
-
-        PACKED_SAND = new EFBlock("packed_sand", Material.SAND, SoundType.SAND, (float) 0.5, 1);
-        PACKED_RED_SAND = new EFBlock("packed_red_sand", Material.SAND, SoundType.SAND, (float) 0.5, 1);
-        PACKED_GRAVEL = new EFBlock("packed_gravel", Material.GROUND, SoundType.GROUND, (float) 0.8, 2);
-
-        NETTLES = new BlockNettles("stinging_nettles");
-
-        NETHER_BRICK_DOOR = new BlockEFDoor("nether_brick_door", Material.ROCK, 1f, 1011, 1005);
-        PURPUR_DOOR = new BlockEFDoor("purpur_door", Material.ROCK, 1f, 1011, 1005);
-
-        REDSTONE_ROD = new BlockRedstoneRod("redstone_rod", Material.CIRCUITS, SoundType.METAL, 0.5f, 0, 13);
-
         STAINED_LAMPS = new BlockStainedLamp[16];
         STAINED_REDSTONE_TORCHES = new BlockStainedRedstoneTorch[16];
         for (int i = 0; i < 16; i++) {
@@ -118,24 +113,39 @@ public class ModBlocks {
             final IForgeRegistry<Block> registry = event.getRegistry();
 
             event.getRegistry().registerAll(
-                    VIEWED_BLOCK,
-                    SMOOTH_GLOWSTONE,
-                    POLISHED_GLOWSTONE,
-                    SNOW_BRICK,
-                    BLOCK_BREAKER,
-                    CRYING_OBSIDIAN,
-                    SPIKE_BLOCK,
-                    BLOCK_PLACER,
-                    DECORATIVE_STONE,
-                    SLATE,
-                    BLAZE_BLOCK,
-                    PACKED_SAND,
-                    PACKED_RED_SAND,
-                    PACKED_GRAVEL,
-                    NETTLES,
-                    NETHER_BRICK_DOOR,
-                    PURPUR_DOOR,
-                    REDSTONE_ROD
+                    new BlockViewedBlock("viewed_block", Material.ROCK, 5, 5),
+                    new EFBlock("smooth_glowstone", Material.GLASS, SoundType.GLASS, 0.5f, 2, 1),
+                    new EFBlock("polished_glowstone", Material.GLASS, SoundType.GLASS, 1, 2, 1),
+                    new EFBlock("snow_brick", Material.CRAFTED_SNOW, SoundType.SNOW, 0.5f, 1),
+                    new BlockBlockBreaker("block_breaker", Material.PISTON, 3, 3),
+                    new BlockCryingObsidian("crying_obsidian", Material.ROCK, 100, 100),
+                    new BlockSpike("spike_block", Material.IRON, 1, 1),
+                    new BlockBlockPlacer("block_placer", Material.ROCK, 5, 5),
+
+                    new EFBlock("decorative_stone", Material.ROCK, SoundType.STONE, 3, 3),
+                    new EFBlock("decorative_andesite", Material.ROCK, SoundType.STONE, 3, 3),
+                    new EFBlock("decorative_diorite", Material.ROCK, SoundType.STONE, 3, 3),
+                    new EFBlock("decorative_granite", Material.ROCK, SoundType.STONE, 3, 3),
+
+                    new EFBlock("cream_bricks", Material.ROCK, SoundType.STONE, 3, 3),
+                    new EFBlock("dirty_bricks", Material.ROCK, SoundType.STONE, 3, 3),
+                    new EFBlock("long_bricks", Material.ROCK, SoundType.STONE, 3, 3),
+                    new EFBlock("blue_bricks", Material.ROCK, SoundType.STONE, 3, 3),
+                    new EFBlock("mixed_bricks", Material.ROCK, SoundType.STONE, 3, 3),
+
+                    new BlockSlate("slate", Material.ROCK, 2, 3),
+                    new BlockBlaze("blaze_block"),
+
+                    new EFBlock("packed_sand", Material.SAND, SoundType.SAND, (float) 0.5, 1),
+                    new EFBlock("packed_red_sand", Material.SAND, SoundType.SAND, (float) 0.5, 1),
+                    new EFBlock("packed_gravel", Material.GROUND, SoundType.GROUND, (float) 0.8, 2),
+
+                    new BlockNettles("stinging_nettles"),
+
+                    new BlockEFDoor("nether_brick_door", Material.ROCK, 1f, 1011, 1005),
+                    new BlockEFDoor("purpur_door", Material.ROCK, 1f, 1011, 1005),
+
+                    new BlockRedstoneRod("redstone_rod", Material.CIRCUITS, SoundType.METAL, 0.5f, 0, 13)
             );
             event.getRegistry().registerAll(STAINED_REDSTONE_TORCHES);
             event.getRegistry().registerAll(STAINED_LAMPS);
@@ -177,15 +187,15 @@ public class ModBlocks {
             }
 
             for (BlockStainedRedstoneTorch torch : STAINED_REDSTONE_TORCHES) {
-                    final ItemBlock item = new ItemBlock(torch, new Item.Properties());
-                    registry.register(item.setRegistryName(torch.getRegistryName()));
-                    ITEM_BLOCKS.add(item);
+                final ItemBlock item = new ItemBlock(torch, new Item.Properties());
+                registry.register(item.setRegistryName(torch.getRegistryName()));
+                ITEM_BLOCKS.add(item);
             }
 
             for (BlockStainedLamp lamp : STAINED_LAMPS) {
-                    final ItemBlock item = new ItemBlock(lamp, new Item.Properties());
-                    registry.register(item.setRegistryName(lamp.getRegistryName()));
-                    ITEM_BLOCKS.add(item);
+                final ItemBlock item = new ItemBlock(lamp, new Item.Properties());
+                registry.register(item.setRegistryName(lamp.getRegistryName()));
+                ITEM_BLOCKS.add(item);
             }
 
         }
