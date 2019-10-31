22d88830d455a41e5831d7a7b3e07cc9006a79b8
williambl <william@williambl.com>
2019-10-31 20:11:32 +0000

    Completely comment out the villager stuff

--- src/main/java/com/williambl/essentialfeatures/common/entity/VillagerMechanic.java
+++ src/main/java/com/williambl/essentialfeatures/common/entity/VillagerMechanic.java
@@ -1,22 +1,14 @@
 package com.williambl.essentialfeatures.common.entity;
 
-import com.google.common.collect.ImmutableSet;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.merchant.villager.VillagerProfession;
-import net.minecraft.village.PointOfInterestType;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-
 public class VillagerMechanic {
 
-    //TODO: Get all this to actually *work*, I've never actually played 1.14 so I have no clue what all this new villager stuff is
-
+    //TODO: Get all this to actually *work*, I've never actually played 1.14 so I have no clue what all this new villager stuff is. Leaving it be for now.
+/*
     protected static final VillagerProfession MECHANIC_PROFESSION = new VillagerProfession("mechanic", new PointOfInterestType("mechanic", ImmutableSet.of(Blocks.REDSTONE_BLOCK.getDefaultState()), 1, null, 1), ImmutableSet.of(), ImmutableSet.of()).setRegistryName("mechanic");
-    /*protected static final VillagerCareer MECHANIC_CAREER = new VillagerRegistry.VillagerCareer(MECHANIC_PROFESSION, "mechanic")
+    protected static final VillagerCareer MECHANIC_CAREER = new VillagerRegistry.VillagerCareer(MECHANIC_PROFESSION, "mechanic")
             .addTrade(1, new EmeraldForItems(Items.REDSTONE, new PriceInfo(20, 1)))
             .addTrade(2, new ListItemForEmeralds(new ItemStack(Blocks.REDSTONE_LAMP), new PriceInfo(1, 3)))
-            .addTrade(2, new ListItemForEmeralds(new ItemStack(ModBlocks.STAINED_LAMPS[4]), new PriceInfo(1, 5)));*/
+            .addTrade(2, new ListItemForEmeralds(new ItemStack(ModBlocks.STAINED_LAMPS[4]), new PriceInfo(1, 5)));
 
     @Mod.EventBusSubscriber
     public static class RegistrationHandler {
@@ -29,4 +21,5 @@ public class VillagerMechanic {
             );
         }
     }
+    */
 }
